a058e29308a100555e7da402728797c312fa4e1a
Neo <qqkkwana@gmail.com>
2024-10-24 22:55:24 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -151,10 +151,10 @@ const ExtendedTheme: Theme = {
     
     // Obtain configuration from: https://giscus.app/
     giscusTalk({
-      repo: 'Jackiexiao/nolebase-template',
-      repoId: 'R_kgDOL5WHsg',
+      repo: 'neo896/nolebase-template',
+      repoId: 'R_kgDONFRV6A',
       category: 'Announcements', // default: `General`
-      categoryId: 'DIC_kwDOL5WHss4CfTYs',
+      categoryId: 'DIC_kwDONFRV6M4Cjp_q',
       mapping: 'url', // default: `pathname`
       inputPosition: 'top', // default: `top`
       lang: 'zh-CN', // default: `zh-CN`
