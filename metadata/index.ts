7cdc63855018bb44ba1371bb56c9d31a03cdd47b
Neo <qqkkwana@gmail.com>
2024-10-24 22:37:13 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'Lifeisbinary'
+export const siteShortName = 'Lifeisbinary'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
-export const include = ['笔记', '生活']
+export const include = ['分享', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/neo896'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+// export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'ob.lifeisbinary.cn'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
